49b46a9cbe7896f8ffc3d57eb12439c2b9040660
Henry Faulkner <hfaulkner27@gmail.com>
2022-06-29 15:14:57 -0400

    Finished implementation of TailwindCss; Added DaisyUI for prebuilt components (henry's use);

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="container">
+  <h1>Welcome to SvelteKit</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+  <input type="checkbox" class="toggle" />
+</div>
