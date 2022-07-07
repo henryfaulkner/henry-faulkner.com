9a60e4ffaff0ce95103758de9158ca9d37860324
Henry Faulkner <hfaulkner27@gmail.com>
2022-07-07 11:07:02 -0400

    Footer smooth scroll

--- src/components/structure/Footer.svelte
+++ src/components/structure/Footer.svelte
@@ -1,3 +1,13 @@
+<script context="module">
+  export function scrollIntoView({ target }) {
+    const el = document.querySelector(target.getAttribute("href"));
+    if (!el) return;
+    el.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+</script>
+
 <footer class="px-4 flex flex-col items-center gap-4 py-5 text-center">
   <span
     class="underline underline-offset-4 max-w-[50ch] mb-10"
@@ -5,22 +15,12 @@
     >This site uses Google Analytics (which adds cookies) to measure the number
     of unique visitors.</span
   >
-  <a
-    href="#index"
-    id="back-to-top"
-    on:click={() => {
-      window.scroll({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-      });
-    }}
-  >
-    <div class="flex items-center gap-2">
+  <div class="flex items-center gap-2">
+    <a href="#index" id="back-to-top" on:click|preventDefault={scrollIntoView}>
       <!-- <Icon name="fluent:arrow-up-24-filled" /> -->
       Back To Top
-    </div>
-  </a>
+    </a>
+  </div>
   <span>© 2022 My Name. All rights reserved.</span>
 </footer>
 
