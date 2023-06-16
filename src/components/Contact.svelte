ed119118ed3530a012dfe25631a48223ace58759
Henry Faulkner <hfaulkner27@gmail.com>
2023-06-15 22:13:27 -0400

    rename resume

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -9,7 +9,7 @@
       GitHub: "https://github.com/henryfaulkner",
       Email: "mailto:hfaulkner27@gmail.com",
       Resume: await getStorageUrl(
-        "resumes/Henry Faulkner - Software Engineer Resume (Late 2022).pdf"
+        "resumes/Henry Faulkner - Software Engineer Resume (Mid 2023).pdf"
       ),
       Blogs: "https://blogs.perficient.com/author/hfaulkner",
       Twitter: "https://twitter.com/HenryFaulknerIO",
