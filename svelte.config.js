60d93558b8ded876d07f10031c46bcf9b0520d8a
Henry Faulkner <hfaulkner27@gmail.com>
2023-07-17 23:36:18 -0400

    remove svimg

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { imagePreprocessor } from 'svimg';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
